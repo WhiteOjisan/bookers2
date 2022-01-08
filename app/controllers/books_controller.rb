9f8d0afef4369c3fef5bddd542a9936ad11014e2
EC2 Default User <ec2-user@ip-172-31-85-123.ec2.internal>
2022-01-08 06:01:50 +0000

    prototype8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @user = current_user
   end
 
   def show
@@ -13,25 +14,37 @@ class BooksController < ApplicationController
   end
 
   def create
+    @user = current_user
+    @books = Book.all
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      flash[:notice] = "successfully created"
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
   end
 
   def edit
+    @books = Book.all
     @book = Book.find(params[:id])
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "successfully updated"
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
+    flash[:notice] = "successfully deleted"
     redirect_to user_path(current_user.id)
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    @user = current_user
   end
 
   def show
@@ -14,9 +15,13 @@ class UsersController < ApplicationController
   end
 
   def update
-    user = User.find(params[:id])
-    user.update(user_params)
-    redirect_to user_path(user.id)
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "successfully updated"
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
   end
 
   private
